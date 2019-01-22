292848bad5b20f12ec2c411f028230102c9c022e
Jameal Ghaznawi <jameal@oldspeake.io>
2019-01-22 17:14:45 -0500

    Make likes/retweets revealable

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Good ol Twitter
 See only your followers' tweets. No likes. No retweets.
+
+## Roadmap
+- Add options page to either hide likes/retweets entirely or display the reveal placeholder
+- Filter certain words or phrases
+- Exclude half of all tweets to weed out the unlucky ones
+
+## Changelog
+- 0.0.2 - Instead of hiding likes/retweets they are obscured with the option to reveal them
+- 0.0.1 – Initial
--- src/inject.js
+++ src/inject.js
@@ -1,23 +1,16 @@
 var readyStateCheckInterval = setInterval(function() {
-
 	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
-
-		setTimeout(processTimeline,100)
+		clearInterval(readyStateCheckInterval)
+		setTimeout(processTimeline, 100)
 
  		let mutationTimer = 0
       let observer = new MutationObserver((mutations) => {
         clearTimeout(mutationTimer)
-				console.log('mutate')
         mutationTimer = setTimeout(processTimeline, 2000)
       })
       observer.observe(document.querySelector('.stream'), { childList: true, subtree: true })
-
-
-
 	}
-
-}, 10);
+}, 10)
 
 function processTimeline() {
 	let tweets = document.querySelectorAll('.tweet')
@@ -25,12 +18,42 @@ function processTimeline() {
 	for(let i=0; i<tweets.length; i++) {
 		let tweet = tweets[i]
 		let isRetweet = tweet.getAttribute('data-retweet-id')
-		let isLikedTweet = tweet.querySelector('.context').querySelector('.Icon--heartBadge')
-
-		if(isRetweet || isLikedTweet) {
-			tweet.style = 'display: none;'
-			//tweet.style = 'opacity: 0.35';
-			console.log('got one')
+		let isLikedTweet = tweet.querySelector('.context') && tweet.querySelector('.context .Icon--heartBadge')
+		let isPristine = !!!tweet.getAttribute('data-good-ol-touched')
+		if(isPristine && (isRetweet || isLikedTweet)) {
+			//tweet.style = 'display: none;'
+			tweet.setAttribute('data-good-ol-touched', true)
+				let frag = document.createRange().createContextualFragment(`
+					<div style="
+						position: absolute;
+				    top: 0;
+				    left: 0;
+				    width: 100%;
+				    height: 101%;
+				    background: #e6ecf0;
+				    z-index: 101;
+				    display: flex;
+				    align-items: center;
+				    justify-content: center;
+						color: rgba(0,0,0,0.5);
+					">
+						Reveal ${(isRetweet) ? 'retweet' : 'liked tweet'}
+					</div>
+				`)
+				tweet.appendChild(frag)
+				tweet.addEventListener("click", revealTweet)
+				tweet.style = `
+					height: 70px;
+				`
 		}
 	}
+}
+
+function revealTweet(Event) {
+	event.preventDefault()
+	event.stopPropagation()
+	let el = event.target
+	el.style = ''
+	el.parentNode.style = ''
+	el.remove()
 }
